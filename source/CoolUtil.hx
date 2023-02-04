54525a680440b88b34e069615bf75d9dd1db5c76
JoseXD <100051827+JoseXD2@users.noreply.github.com>
2023-02-04 16:09:12 -0300

    Update CoolUtil.hx

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -62,7 +62,7 @@ class CoolUtil
 	public static function coolTextFile(path:String):Array<String>
 	{
 		var daList:Array<String> = [];
-		#if windows
+		#if 
 		if(Tools.exists(path)) daList = Tools.getContent(path).trim().split('\n');
 		#else
 		if(Assets.exists(path)) daList = Assets.getText(path).trim().split('\n');
